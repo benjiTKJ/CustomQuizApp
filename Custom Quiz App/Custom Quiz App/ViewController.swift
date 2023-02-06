04cb95549ae4e2a2227b055a7ec1e9680218c106
Benjamin Tan <bentankj@gmail.com>
2023-02-06 16:59:43 +0800

    Adding inital files

---
+++ Custom Quiz App/Custom Quiz App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Custom Quiz App
+//
+//  Created by Brendan Tan on 6/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
